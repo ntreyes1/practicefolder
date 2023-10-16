d25dfacd747fd71aef2044f430012f519bffd6b7
Nick Reyes <nickreyes@Nicks-MacBook-Pro-3.local>
2023-10-16 15:56:03 -0500

    now?

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ const port = process.env.PORT || 3000;
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
