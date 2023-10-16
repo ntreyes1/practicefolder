fd4b149332e0f84a81572a0721bbe3a5b6372845
Nick Reyes <nickreyes@Nicks-MacBook-Pro-3.local>
2023-10-16 15:53:50 -0500

    fingerscrossed

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('<html><body><h1>Hello, World!</h1></body></html>');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('<html><body><h1>Hello, World!</h1></body></html>');
-});
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
