575df91940032c787b49c36a3dea6d6f3866f088
Nick Reyes <nickreyes@Nicks-MacBook-Pro-3.local>
2023-10-16 15:12:20 -0500

    make it better

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('<html><body><h1>Hello, World!</h1></body></html>');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Import the myExercises variable from index.js
+const myExercises = require('./index.js');
+
 app.get('/', (req, res) => {
-  res.send('<html><body><h1>Hello, World!</h1></body></html>');
+  // Use myExercises to generate the HTML content
+  let html = `
+    <html>
+      <body>
+        <h1>My Exercises</h1>
+        <ul>
+  `;
+
+  myExercises.forEach((exercise) => {
+    html += `
+          <li>Exercise: ${exercise.exercise}</li>
+          <li>Distance: ${exercise.distance} miles</li>
+          <li>Date: ${exercise.date}</li>
+          <br>
+    `;
+  });
+
+  html += `
+        </ul>
+      </body>
+    </html>
+  `;
+
+  res.send(html);
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
